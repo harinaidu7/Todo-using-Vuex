8280a0706da4d3160a8faa69d89d183241b38827
Krishnan <krishnannaidu704@gmail.com>
2022-09-05 12:17:37 +0530

    added responsiveness

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
         <h1>Todo list</h1>
+        <div class="legend">
+            <span>
+                Double click to mark as complete
+            </span>
+            <span>
+                <span class="incomplete-box"></span> = Incomplete
+            </span>
+            <span>
+                <span class="complete-box"></span>= Complete
+            </span>
+        </div>
         <div class="todos">
             <div v-for="todo in allTodos" :key="todo.id" class="todo"  >
                 {{todo.title}}
@@ -52,6 +63,29 @@
         color: rgb(103, 6, 6);
         cursor: pointer 
 
+    }
+    .legend{
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 1rem;
+    }
+
+    .complete-box{
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background:#35495e;
+    }
+    .incomplete-box{
+        display: inline-block;
+        width:10px;
+        height:10px;
+        background: #41b883 
+    }
+    @media(max-width: 500px){
+        .todos{
+            grid-template-columns:1fr 
+        }
     }
   
 
